f395d6e0b2ca249db814bf16593691022114ace1
enan7 <enan7@hotmail.com>
2022-09-25 02:22:04 +0500

    Initial setup

---
+++ parent_doctor_cattle/src/test/java/com/doctorcattle/parent/parent_doctor_cattle/ParentDoctorCattleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doctorcattle.parent.parent_doctor_cattle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParentDoctorCattleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
